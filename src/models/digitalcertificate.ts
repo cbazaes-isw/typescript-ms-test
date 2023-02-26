ef8793b7d32da6dc4f33f23e2162530e7aaf593d
Cristian Bazaes <cbazaes.isw@gmail.com>
2023-02-26 13:13:26 -0300

    feat (digital certificate) avance

--- src/models/digitalcertificate.ts
+++ src/models/digitalcertificate.ts
@@ -1,9 +1,14 @@
 import * as mongoose from 'mongoose';
+import { Schema } from 'mongoose';
 
 const DigitalCertificateSchema = new mongoose.Schema({
     rut: { type: String },
+    sujeto: { type: String },
+    email: { type: String },
+    organizacion: { type: String },
     fechaEmision: { type: Date },
     fechaExpiracion: { type: Date },
+    info: { type: Schema.Types.Mixed },
     content: { type: Buffer }
 });
 
